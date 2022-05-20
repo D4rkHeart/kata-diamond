785d0271cfce3ea36beed8235d5e3548c1fc719d
D4rkHeart <leonardo.surdez@gmail.com>
2022-05-20 11:11:18 +0200

    [Feature] Added user input

--- PythonDiamond.py
+++ PythonDiamond.py
@@ -1,2 +1,3 @@
 space = " "
 letter = ["A","B","C","D","E","F","G","H","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+selChar = input("Enter the letter that will define the size of the diamond: ")
