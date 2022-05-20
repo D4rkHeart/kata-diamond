00eddf025eb5d72c8549bb683a0d171eb15d0e93
D4rkHeart <leonardo.surdez@gmail.com>
2022-05-20 11:12:27 +0200

    [Feature] Added user input verification

--- PythonDiamond.py
+++ PythonDiamond.py
@@ -1,3 +1,10 @@
 space = " "
 letter = ["A","B","C","D","E","F","G","H","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 selChar = input("Enter the letter that will define the size of the diamond: ")
+
+#Verification of the user input
+try:
+	x = letter.index(selChar)
+except:
+	print("Please use only capital letter") 
+	exit()
